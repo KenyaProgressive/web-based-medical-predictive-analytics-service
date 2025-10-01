3d8a68bc58b667e728041fd819eee8cea03aa54a
dolgovv <dolgovv.dev@gmail.com>
2025-10-01 15:15:34 +0300

    front moved to React Query. Animations added

---
+++ app/frontend/fetal-frontend/src/components/view/DefaultStates.js
@@ -0,0 +1,34 @@
+export const defaultDashboard = {
+    'card_params': [{
+        'title': "-",
+        'value': 1,
+        'status': 0,
+        'rows': 2,
+        'cols': 2,
+        'id': 1
+    },
+    {
+        'title': "-",
+        'value': 1,
+        'status': 0,
+        'rows': 1,
+        'cols': 1,
+        'id': 2
+    },
+    {
+        'title': "-",
+        'value': 1,
+        'status': 0,
+        'rows': 1,
+        'cols': 1,
+        'id': 3
+    },
+    {
+        'title': "-",
+        'value': 1,
+        'status': 0,
+        'rows': 1,
+        'cols': 2,
+        'id': 4
+    }]
+};
